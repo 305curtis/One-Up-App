ad4a325c1b61dbc46c2b83ca247bf894515b357f
Rosenthol Curtis <rosentholcurtis@gmail.com>
2018-11-28 15:17:41 -0500

    update landing page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,10 +8,18 @@ class Home extends Component {
         return (
             <div>
                 <Navbar />
-                <div className="container topped">
-                <h2>Welcome to the best place on earth.</h2>
-                <p>This is where all of your dreams come true.</p>
+                <div className="container topped text-center">
+                <div className="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
+                <main role="main" className="inner cover">
+                    <h1 className="cover-heading">Enable One-Up Security</h1>
+                    <p className="lead">The One-Up security feature gives credit/debit card users the ability to accept of deny purchases via the cell-phone/device using the one-up feature.</p>
+                    <p className="lead">
+                    <a href="/Settings" class="btn btn-lg btn-secondary">Get Started</a>
+                    </p>
+                </main>
                 </div>
+                </div>
+                <div className="container"></div>
                 <Footer />
             </div>
         );
